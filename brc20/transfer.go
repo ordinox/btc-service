bfa51e744377af05c2cfc4ba0d6df02a0958944b
Ashwin Prasad <ashwin@onpar.in>
2024-05-05 21:04:19 +0530

    updated inscriptions to use big.Float

--- brc20/transfer.go
+++ brc20/transfer.go
@@ -37,7 +37,7 @@ func InscribeTransfer(ticker string, amt *big.Float, destination btcutil.Address
 		P:    "brc-20",
 		Op:   "transfer",
 		Tick: ticker,
-		Amt:  fmt.Sprintf("%d", amt),
+		Amt:  amt.String(),
 	}
 	bz, _ := json.Marshal(transfer)
 	inscription := taproot.NewInscriptionData(string(bz), taproot.ContentTypeText)
